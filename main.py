e319cd8082bb308858e96a459c244a061ed2501f
Mayur <ms9004364918@gmail.com>
2026-05-20 22:50:09 +0200

    Fixed Streamlit deployment

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import re
 import spacy
 
 # Load NLP model
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    spacy.cli.download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 # Streamlit Config
 st.set_page_config(
